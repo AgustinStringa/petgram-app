548c959f071bce89287db76c95053daa0a898906
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-05 14:17:06 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
 console.log('HOLAAAA');
+console.log('HIIIIIIII');
 const root = createRoot(document.querySelector("#app")); // createRoot(container!) if you use TypeScript
 root.render(<h1>'HOLAA PLATZI'</h1>);
