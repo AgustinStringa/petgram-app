e0575e5402a415a0e84bf6242fdc69f58645beb8
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-05 09:59:07 -0300

    edit settings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
-
+console.log('HOLAAAA')
 const root = createRoot(document.querySelector("#app")); // createRoot(container!) if you use TypeScript
 root.render(<h1>'HOLAA PLATZI'</h1>);
